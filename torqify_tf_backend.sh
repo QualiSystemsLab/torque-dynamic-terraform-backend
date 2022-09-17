afb549658253af1865705318042075ce4a425300
alexazarh <aazarh@gmail.com>
2022-09-17 14:06:32 -0500

    fix build errors

--- torqify_tf_backend.sh
+++ torqify_tf_backend.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # needs to run in debian
 
+# exit when any command fails
+set -e
+
 version=<VERSION>
-package_url=https://github.com/QualiSystemsLab/torque-dynamic-terraform-backend/releases/download/v<VERSION>/cloudshell-email-<VERSION>.tar.gz
+package_url="https://github.com/QualiSystemsLab/torque-dynamic-terraform-backend/releases/download/v$version/torqify_tf_backend.$version.tar.gz"
 
 # get environment id from cli args
 env_id=$1
+echo "===> Environment id: $env_id"
 
 # install curl
 echo "===> Installing curl"
-apt update
-apt install curl
+apt update -y
+apt install curl -y
 
 # download python package
-echo "===> Downloading package"
-curl -s -L  -o torqify_tf_backend.tar.gz
+echo "===> Downloading package from url: $package_url"
+curl -L $package_url -o torqify_tf_backend.tar.gz
 
 # extract package
 echo "===> Extracting package"

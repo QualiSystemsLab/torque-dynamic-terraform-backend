8c33e4303f971eea85e9b8e6fffd8b8f92fcf82a
alexazarh <aazarh@gmail.com>
2022-09-19 09:49:03 -0500

    fixing CD

--- build.sh
+++ build.sh
@@ -7,10 +7,7 @@ cd src
 tar -czf ../dist/torqify_tf_backend.$version.tar.gz .
 cd ..
 
-cp ./runner/torqify_tf_backend.sh ./dist/torqify_tf_backend.$version.sh
-cat ./dist/torqify_tf_backend.$version.sh | sed -e "s/<VERSION>/${version}/g" > ./dist/torqify_tf_backend2.$version.sh
+cat ./runner/torqify_tf_backend.sh | sed -e "s/<VERSION>/${version}/g" > ./dist/torqify_tf_backend.$version.sh
 
-echo "===> debugging"
+echo "===> list dist folder"
 ls -l ./dist
-echo "print torqify_tf_backend.sh for debugging"
-cat ./dist/torqify_tf_backend.$version.sh

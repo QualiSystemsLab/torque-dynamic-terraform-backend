57d92097b58c983ed20856f43a3fc4f6ba5ee767
alexazarh <aazarh@gmail.com>
2022-09-19 09:41:00 -0500

    fixing CI

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ tar -czf ../dist/torqify_tf_backend.$version.tar.gz .
 cd ..
 
 cp ./runner/torqify_tf_backend.sh ./dist/torqify_tf_backend.$version.sh
-cat ./dist/torqify_tf_backend.$version.sh | sed -e "s/<VERSION>/${version}/g" > ./dist/torqify_tf_backend.$version.sh
+cat ./dist/torqify_tf_backend.$version.sh | sed -e "s/<VERSION>/${version}/g" > ./dist/torqify_tf_backend2.$version.sh
 
 echo "===> debugging"
 ls -l ./dist

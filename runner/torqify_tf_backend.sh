7dae2338203a881c63cc746979e8e476c9f249c9
alexazarh <aazarh@gmail.com>
2022-10-12 13:41:58 -0500

    removed curl install. No longer needed because curl is now part of the Torque TF runner
    bump minor  version

--- runner/torqify_tf_backend.sh
+++ runner/torqify_tf_backend.sh
@@ -7,13 +7,8 @@ set -e
 version=<VERSION>
 package_url="https://github.com/QualiSystemsLab/torque-dynamic-terraform-backend/releases/download/v$version/torqify_tf_backend.$version.tar.gz"
 
-# install curl
-echo "===> Installing curl"
-apt update -y
-apt install curl -y
-
 # download python package
-echo "===> Downloading package from url: $package_url"
+echo "===> Downloading 'torqify_tf_backend' package from url: $package_url"
 curl -L $package_url -o torqify_tf_backend.tar.gz
 
 # extract package
